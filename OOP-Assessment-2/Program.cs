5c1977803ba4eb247f8444489e431e1420698531
Adam Dodds <40017235@student.northlindsey.ac.uk>
2020-03-27 10:24:01 +0000

    Done

---
+++ OOP-Assessment-2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace OOP_Assessment_2
+{
+	//main menu asks the user for set inputs and stores them for later use.
+	//Console.Clear is used throughout to give a cleaner view for the user.
+	class Main_Menu
+	{
+		static void Main(string[] args)
+		{
+			//sets up each file path as a string 
+			string path1 = @"file_a.txt";
+			string path2 = @"file_b.txt";
+
+			//adds the string path above with a corresponding number to a list 
+			txt_File.Add_File_Path(path1, 1);
+			txt_File.Add_File_Path(path2, 2);
+
+			//Gets the user's input on which first file to use.
+			//The error handling ensures only the correct input can be passed, any errors are shown with a error message.
+			int file_select1 = -1;
+			do
+			{
+				Console.WriteLine("Which first File to run " + "\n" + "1. file_a" + "\n" +
+																	  "2. file_b");
+				Console.WriteLine();
+				Console.WriteLine("Type in the number then press enter");
+
+				if (!int.TryParse(Console.ReadLine(), out file_select1))
+				{
+					Console.Clear();
+					Console.WriteLine("Must enter 1 or 2");
+					Console.WriteLine();
+				}
+				else
+				{
+					if (file_select1 < 1 || file_select1 > 2)
+					{
+						Console.Clear();
+						Console.WriteLine("Must enter 1 or 2");
+						Console.WriteLine();
+
+					}
+				}
+			} while (file_select1 < 1 || file_select1 > 2);
+
+			Console.Clear();
+
+			//Gets the user's input on which second file to use.
+			//The error handling ensures only the correct input can be passed, any errors are shown with a error message.
+			int file_select2 = -1;
+			do
+			{
+				Console.WriteLine("Which second File to run " + "\n" + "1. file_a" + "\n" +
+																	   "2. file_b");
+				Console.WriteLine();
+				Console.WriteLine("Type in the number then press enter");
+
+				if (!int.TryParse(Console.ReadLine(), out file_select2))
+				{
+					Console.Clear();
+					Console.WriteLine("Must enter 1 or 2");
+					Console.WriteLine();
+				}
+				else
+				{
+					if (file_select2 < 1 || file_select2 > 2)
+					{
+						Console.Clear();
+						Console.WriteLine("Must enter 1 or 2");
+						Console.WriteLine();
+					}
+				}
+			} while (file_select2 < 1 || file_select2 > 2);
+
+			Console.Clear();
+
+			//calls the file_diff method in txt_File class and passes on the inputted values and returns true or false.			
+			bool diff = txt_File.file_diff(file_select1, file_select2);
+
+			if (diff == true)       //if diff is true print the following
+			{
+				Console.WriteLine("First file picked " + txt_File.Show_txt_File(file_select1) + " is the same as the second file picked " + txt_File.Show_txt_File(file_select2));
+			}
+			else                    //if diff is false print the following
+			{
+				Console.WriteLine("First file picked " + txt_File.Show_txt_File(file_select1) + " is not the same as the second file picked " + txt_File.Show_txt_File(file_select2));
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Press Enter to Exit");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ OOP-Assessment-2/txt_File.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace OOP_Assessment_2
+{
+    class txt_File
+    {
+        //sets up a private list 
+        private static List<txt_File> file_path = new List<txt_File>();
+
+        //setting up constructors 
+        private string Path_File { get; }
+        private int Path_Number { get; }
+
+        private txt_File(string path_file, int path_number)
+        {
+            Path_File = path_file;
+            Path_Number = path_number;
+        }
+
+        //adds string path plus a path number to a list. 
+        public static void Add_File_Path(string path_file, int path_number)
+        {
+            var addFile = new txt_File(path_file, path_number);
+            file_path.Add(addFile);
+        }
+
+        //A foreach is used with the list it matches file_select to the Path_Number. It then reads the matching txt file 
+        public static string[] Get_txt_File(int file_select)
+        {
+            foreach (var file in file_path)
+            {
+                if (file_select == file.Path_Number)                     //Matches user input to the path number
+                {
+                    string[] array = File.ReadAllLines(file.Path_File);  //Makes a string array by reading all lines in the file
+                    return array;                                        //returns the array
+                }
+            }
+            return null;
+        }
+
+        //returns the file name when called
+        public static string Show_txt_File(int file_select)
+        {
+            foreach (var file in file_path)
+            {
+                if (file_select == file.Path_Number)            //Matches user input to the path number
+                {
+                    string file_name = file.Path_File;          //sets file_name to Path_File
+                    return file_name;                           //returns the array
+                }
+            }
+            return null;
+        }
+
+        //Reads in both files by using Get_txt_File method by passing on the file_select
+        //when runs a series of tests which returns false if not the same and true if the same. 
+        public static bool file_diff(int file_select1, int file_select2)
+        {
+            string[] first_file = Get_txt_File(file_select1);
+            string[] second_file = Get_txt_File(file_select2);
+
+            if (first_file.Length == second_file.Length)        //tests if first is the same length as the second file
+            {
+                for (int i = 0; i < first_file.Length; i++)     //runs a loop and tests each index element 
+                {
+                    if (first_file[i] != second_file[i])        //if element is not the same returns false
+                    {
+                        return false;
+                    }
+                }
+                return true;                                    //returns true if all elements match
+            }
+            return false;                                       //returns false if length is not the same 
+        }
+    }
+}
